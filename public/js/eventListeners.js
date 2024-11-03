2aa793ba8a7445cb3c66a034e5741c3edf75f61c
subbu-25 <udathasubhash935@gmail.com>
2024-11-03 15:46:31 +0530

    Add files via upload

---
+++ public/js/eventListeners.js
@@ -0,0 +1,21 @@
+addEventListener('click', (event) => {
+  const canvas = document.querySelector('canvas')
+  const { top, left } = canvas.getBoundingClientRect()
+  const playerPosition = {
+    x: frontEndPlayers[socket.id].x,
+    y: frontEndPlayers[socket.id].y
+  }
+
+  const angle = Math.atan2(
+    event.clientY - top - playerPosition.y,
+    event.clientX - left - playerPosition.x
+  )
+
+  socket.emit('shoot', {
+    x: playerPosition.x,
+    y: playerPosition.y,
+    angle
+  })
+
+  console.log(frontEndProjectiles)
+})
